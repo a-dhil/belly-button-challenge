6a8df7eb6363b1d869a2cad6322b280ce0bae16c
a-dhil <angaddeepdhillon@Angaddeeps-MacBook-Air.local>
2023-11-25 23:08:53 -0500

    two graphs created

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,43 @@
+// Define the URL for JSON data
 const url="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+// Fetch the json data using d3.json
 d3.json(url).then(function(data){
     console.log(data);
 
-
+// Select the first sample from sample array to get graphs
 let sample= data.samples[0];
 console.log(`This is a sample ${sample}`);
+
+// Extract sample values and OTU vaues chnage to format OTU ${otu_id}
 let values=sample.sample_values;
 console.log(`This is value ${values}`);
-//let labels=sample.otu_ids;
-
-//let labels = sample.otu_ids;
 let labels = sample.otu_ids.map(otu_id => `OTU ${otu_id}`);
 console.log(`This is otu_ids ${labels}`);
+let labelBubble = sample.otu_ids;
+console.log(`This is otu_ids used for bubble chart ${labelBubble}`);
  
-let data1=[{
+// Bar graph trace
+let trace1=[{
         y: labels,
         x: values,
         type: 'bar',
         orientation:'h'
         }];
-        Plotly.newPlot('bar', data1); 
+         
+    // Bubble grapg trace
+    let trace2={
+        x: labelBubble,
+        y: values,
+        mode: 'markers',
+        marker: {
+            size: values,
+            color: labelBubble
+        }
+        
+          };
+        Plotly.newPlot('bar', trace1);
+        Plotly.newPlot('bubble', [trace2]);
+        
     })
-    
 
